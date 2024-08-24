5a42be9eb7108e267c6728111d3339c052b236de
bbigemoloser <s3818552@rmit.edu.vn>
2024-08-24 15:39:55 +0700

    404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./pages/onboarding/LoginPage";
 import SignupPage from "./pages/onboarding/SignupPage";
 import RoleRouting from "./utils/RoleRouting";
 import HomePage from "./pages/home/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/dashboard" element={<RoleRouting />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/error" element={<ErrorPage />} />
         </Routes>
       </AuthProvider>
     </Router>
