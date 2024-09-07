9e844ed7aafc6bab3f6a640c917da4b865617f80
kieuahn <s3818552@rmit.edu.vn>
2024-09-07 19:10:35 +0700

    change to test env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "11022003",
+  password: "0925747216aeE",
   database: "hospital_management"
 });
 
